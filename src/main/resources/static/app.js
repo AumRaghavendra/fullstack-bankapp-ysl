b2d00379b03eb00df538184e7fcc9cdff62d432b
Raghav <aumraghavendras@gmail.com>
2026-04-04 22:57:19 +0530

    Fix https API URL

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,4 +1,4 @@
-const API = 'http://fullstack-bankapp-ysl-production.up.railway.app';
+const API = 'https://fullstack-bankapp-ysl-production.up.railway.app';
 let token = localStorage.getItem('token');
 
 // Check if already logged in
